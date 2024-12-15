c083f161a71e52fe167097eca93f811b37667cd3
Bakhtawar <bakhtawarsmit@gmail.com>
2024-12-15 22:19:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+/*alert( " javascript " );
+alert(1213);
+var myName = "java";
+console.log(-myName);
+var $newName = "script";
+console.log($newName);
+var num = 11 + 12;
+console.log(num);
+var num2 = 20*5;
+console.log(num2);
+var num3 = 12/2;
+console.log(num3);
+var num4 = 32%12;
+console.log(num4);
+var num5 = 12<15;
+console.log(num5);
+var num6 = 12>15;
+console.log(num6);
+var age = prompt("your age!");
+console.log(age);
+var cities = [ "karachi", "lahore", " peshawer", "quetta"];
+cities.pop();
+cities.push("faislabad");
+cities.shift();
+cities.unshift();
+if(cities==="karachi"){
+    alert("okay!")
+}else{
+    alert("incorrect")
+}
+console.log(cities);
+
+for(var i = 0; i<=5; i++){
+    console.log(i)
+}
+
+//WHILE LOOP
+var names123 = ["sana", "ali","sara","amna"];
+while(names)
+
+//DO WHILE
+var names23 = ["sana", "ali","sara","amna"];
+do{
+    console.log(names23);names++
+}while(names23>=5); */
+
+
+//LOGIN FORM
+
+function myForm(){
+    var myPassword = document.getElementById("password");
+    var myEmail = document.getElementById("email");
+    console.log( myPassword.value, myEmail.value)
+}
